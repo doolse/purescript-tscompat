ca5a25a1da052a07c70c0fe06e3afc5aa861cab5
Jolse Maginnis <doolse@gmail.com>
2018-09-24 21:41:55 +1000

    Don't use TypeEquals

--- src/Data/TSCompat/Class.purs
+++ src/Data/TSCompat/Class.purs
@@ -4,13 +4,13 @@ import Data.Nullable (Nullable)
 import Data.TSCompat (Any, OneOf, OptionRecord)
 import Data.TSCompat.React (ReactNode)
 import Effect (Effect)
-import Effect.Uncurried (EffectFn1)
+import Effect.Uncurried (EffectFn1, EffectFn2)
 import Prim.Row as Row
 import Prim.RowList as RL
-import React (class IsReactElement)
+import React (class IsReactElement, ReactElement)
 import Type.Data.Boolean as B
 import Type.Data.Symbol as Sym
-import Type.Equality (class TypeEquals)
+import Unsafe.Coerce (unsafeCoerce)
 
 class TsTypeExists t (rl :: RL.RowList) (opt:: B.Boolean) (o :: B.Boolean) | t rl -> o
 instance consCheck :: (IsEq t t2 opt eq, TsTypeExists t tail opt tailEq, B.Or eq tailEq out) 
@@ -22,9 +22,9 @@ instance consAll :: (ConstainsAll tail b, Row.Cons s any btail b) => ConstainsAl
 instance nilAll :: ConstainsAll RL.Nil b 
 
 {-- By using this class we can get pretty good error messages --}
-class TSCompatible (s :: Symbol) ta tb (b :: B.Boolean)
+class TSCompatible (s :: Symbol) ta tb (b :: B.Boolean) | ta -> tb, tb -> ta
 instance onlyTrue :: TSCompatible s ta tb B.True
-instance tryAgain :: TypeEquals a b => TSCompatible s a b B.False
+instance sameType :: TSCompatible s ta ta B.False
 
 class IsOptional (s :: Symbol) (m :: RL.RowList) (b :: B.Boolean) | s m -> b 
 instance consOpt :: (Sym.Equals s s2 eq, B.Not eq neq, IsOptional s tail tailopt, B.And neq tailopt opt) => IsOptional s (RL.Cons s2 any tail) opt
@@ -41,8 +41,11 @@ else instance anyIsEq :: IsEq a Any o B.True
 else instance unionIsEq :: (RL.RowToList b rl, TsTypeExists a rl o eq) 
     => IsEq a (OneOf b) o eq
 else instance intIsNumber :: IsEq Int Number o B.True
+else instance nullElem :: IsEq (Nullable ReactElement) ReactNode o B.True
 else instance reactElement :: IsReactElement a => IsEq a ReactNode o B.True
 else instance effectAsFn1 :: IsEq (Effect a) (EffectFn1 e a) o B.True
+else instance effectAsFn2 :: IsEq (Effect a) (EffectFn2 e b a) o B.True
+else instance effectFn1asFn2 :: IsEq (EffectFn1 e a) (EffectFn2 e b a) o B.True
 else instance typeConsEq :: IsEq a b o eq => IsEq (m a) (m b) o eq
 else instance optRecord :: (RL.RowToList a al, RL.RowToList mand ml, 
     IsEqRowList al all ml, ConstainsAll ml a) => IsEq (Record a) (OptionRecord all mand) o B.True
@@ -51,3 +54,6 @@ else instance notEq :: IsEq a b o B.False
  
 class IsTSEq a b 
 instance anyTSEq :: IsEq a b B.False B.True => IsTSEq a b
+
+asTS :: forall a b. IsTSEq a b => a -> b 
+asTS = unsafeCoerce
--- src/Data/TSCompat/React.purs
+++ src/Data/TSCompat/React.purs
@@ -1,3 +1,9 @@
 module Data.TSCompat.React where 
 
+import React (ReactClass, ReactElement, unsafeCreateElement) as R
+import Unsafe.Coerce (unsafeCoerce)
+
 foreign import data ReactNode :: Type 
+
+unsafeCreateElement :: forall clz props child. R.ReactClass clz -> props -> child -> R.ReactElement
+unsafeCreateElement = unsafeCoerce R.unsafeCreateElement
